648325caf3e8c12228dd7ccb82f8713a6f564892
chynkim <101403570+chynkim@users.noreply.github.com>
2022-03-28 08:51:51 +0900

    이온 4주차 과제

--- 소스.cpp
+++ 소스.cpp
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
-int tot(int* parr);
 
 int main()
-{
-	int arr[6], i;
+{	
+	int arr[6], i, j, n, t, sw;
+	n = 6;
 
-	printf("enter numbers : ");
+	printf("숫자를 6개 입력하세요 : ");
 
-	for (i = 0; i < 6; i++)
-		scanf_s("%d,", &arr[i]);
+	for(i=0; i<n; i++)
+		scanf_s("%d", &arr[i]);
 
-	printf("%d\n", tot(arr));
-}
-
-int tot(int* parr)
-{
-	int n, i;
-
-	n = 0;
-	for (int i = 0; i < 6; i++)
-		n = n + *parr++;
-
-	return n;
+	for (i = 0; i < n - 1; i++) {
+		sw = 0;
+		for (j = 1; j < n ; j++)
+			if (arr[j - 1] > arr[j]) {
+				t = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = t;
+				sw = 1;
+			}	
+		if (sw == 0)
+			break;
+	}
+	printf("[");
+	for (i = 0; i < n; i++) {
+		printf("%4d", arr[i]);
+		if (i < n)
+			printf(", ");
+	}
+	printf("]");
 }
